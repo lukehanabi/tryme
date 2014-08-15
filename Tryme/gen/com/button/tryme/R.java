b59e4b6bcab27315ac4e2fdc87beb2dc3fedbfae
lukehanabi <lucas.luduena@gmail.com>
2014-08-15 03:58:02 -0300

    Recording Branch

--- Tryme/gen/com/button/tryme/R.java
+++ Tryme/gen/com/button/tryme/R.java
@@ -12,16 +12,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int button1=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int cross=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int recbutton=0x7f020003;
+        public static final int stop=0x7f020004;
     }
     public static final class id {
         public static final int imageView1=0x7f060000;
+        public static final int imageView2=0x7f060002;
+        public static final int text1=0x7f060001;
+        public static final int text2=0x7f060003;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int removeButton=0x7f040001;
     }
     public static final class style {
         /** 
--- Tryme/src/com/button/tryme/FullscreenActivity.java
+++ Tryme/src/com/button/tryme/FullscreenActivity.java
@@ -1,10 +1,21 @@
 package com.button.tryme;
 
+import java.io.File;
+import java.io.IOException;
+
+
+
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,7 +29,15 @@ public class FullscreenActivity extends Activity {
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
 	 */
 	int COUNTER = 1;
+	boolean RECORD_ON = false;
+	private static String ENV = null;
 	MediaPlayer mp = null;
+	MediaRecorder mr = null;
+	CountDownTimer count = null;
+	
+	public FullscreenActivity() {
+		ENV = Environment.getExternalStorageDirectory().getAbsolutePath();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +52,9 @@ public class FullscreenActivity extends Activity {
 	
 	public void playSound(View view) { 
 		
+		ImageView image = (ImageView)findViewById(R.id.imageView1);
+    	image.setImageResource(R.drawable.cross);
+    	
 		 if (mp != null && mp.isPlaying()) {
              mp.stop();
              mp.release();
@@ -47,18 +69,121 @@ public class FullscreenActivity extends Activity {
 	    	 COUNTER = 1;
 	     } 
 
-	    	AssetFileDescriptor descriptor = getAssets().openFd("sound"+(COUNTER++)+".mp3");
-	        mp.setDataSource(descriptor.getFileDescriptor(),descriptor.getStartOffset(), descriptor.getLength());    
+	     
+	    	AssetFileDescriptor descriptor = null;
+	    	boolean newsoundExists = false;
+	    	try {
+	    	  File f = new File(ENV + "/newsound"+(COUNTER)+".mp3");
+	    	  
+	    	  if (f.exists()) {
+	    	    mp.setDataSource(ENV + "/newsound"+(COUNTER)+".mp3");
+	    	    newsoundExists = true;
+
+		    	TextView text = (TextView)findViewById(R.id.text2);
+		    	text.setText("newsound"+(COUNTER)+".mp3");
+	    	  }
+	    	  
+	    	} catch (Exception e) {
+		        e.printStackTrace();
+		    }
+	    	
+	    	if (newsoundExists == false) {
+	    		descriptor = getAssets().openFd("sound"+(COUNTER)+".mp3");
+	    		TextView text = (TextView)findViewById(R.id.text2);
+		    	text.setText("sound"+(COUNTER)+".mp3");
+	    		mp.setDataSource(descriptor.getFileDescriptor(),descriptor.getStartOffset(), descriptor.getLength());
+	          }
+	    	
+  
 	        mp.prepare();
 	        mp.start();
+	        COUNTER++;
+	        image = (ImageView)findViewById(R.id.imageView1);
+	    	image.setImageResource(R.drawable.button1);
 	        
 	    } catch (Exception e) {
 	        e.printStackTrace();
 	    }
 }  
-
-
 	
+	public void onRecordClick(View view) { 
+		
+		
+		
+		 if (RECORD_ON == false) {
+	            startRecording();
+	            ImageView image = (ImageView)findViewById(R.id.imageView2);
+		    	image.setImageResource(R.drawable.stop);
+	        } else {
+	            stopRecording();
+	            ImageView image = (ImageView)findViewById(R.id.imageView2);
+		    	image.setImageResource(R.drawable.recbutton);
+	        }
+}  
+
+
+	 public void startRecording() {
+	        mr = new MediaRecorder();
+	        mr.setAudioSource(MediaRecorder.AudioSource.MIC);
+	        mr.setMaxDuration(10000);
+	        mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+	        mr.setAudioEncodingBitRate(128);
+	        mr.setOutputFile(ENV + "/newsound"+(COUNTER)+".mp3");
+	        mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+	        try {
+	            mr.prepare();
+	        } catch (IOException e) {
+	            Log.e("", "prepare() failed");
+	        }
+	        mr.start();
+	        
+	        
+	        
+	        count = new CountDownTimer(10000, 1000) {
+	        	
+	        	TextView text = (TextView)findViewById(R.id.text1);
+
+	            public void onTick(long millisUntilFinished) {
+	            	
+	            	
+	            	text.setText("Secs left: " + millisUntilFinished / 1000);
+	            }
+
+	            public void onFinish() {
+	                text.setText("done!");
+	            }
+	         }.start();
+	         
+	        RECORD_ON = true;
+	    }
+
+	    private void stopRecording() {
+	    	count.cancel();
+	    	TextView text = (TextView)findViewById(R.id.text1);
+	    	text.setText("done!");
+	    	ImageView image = (ImageView)findViewById(R.id.imageView1);
+	    	image.setImageResource(R.drawable.button1);
+	        mr.stop();
+	        mr.release();
+	        mr = null;
+	        RECORD_ON = false;
+	    }	
+	    
+	    public void removeSound(View view) { 
+	    	try {
+		    	  File f = new File(ENV + "/newsound"+(COUNTER-1)+".mp3");
+		    	  
+		    	  if (f.exists()) {
+		    	    f.delete();
+		    	    TextView text = (TextView)findViewById(R.id.text2);
+			    	text.setText("Done!! newsound"+(COUNTER-1)+".mp3");
+		    	  }
+		    	  
+		    	} catch (Exception e) {
+			        e.printStackTrace();
+			    }
+			 }
 
 
 }
